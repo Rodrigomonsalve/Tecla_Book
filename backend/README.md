3a6a60b5675d0a8640f824731b5bd335059f6baa
Rodrigomonsalve <94495911+Rodrigomonsalve@users.noreply.github.com>
2022-01-23 19:36:22 -0600

    23 de enero de 2022 3

--- backend/README.md
+++
@@ -1,1 +0,0 @@
-# Tecla_Book
--- backend/conection/db.js
+++
@@ -1,9 +0,0 @@
-let Sequelize = require ("sequelize");
-
-const sequelize = new Sequelize ("usuarios", "root", null, {
-    host:"localhost",
-    dialect:"mysql",
-    port:3306
-})
-
-module.exports=sequelize;
