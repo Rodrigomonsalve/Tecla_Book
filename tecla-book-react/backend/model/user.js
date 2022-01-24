ebd4d953239ed470daf1065e13889b93d77801be
Rodrigomonsalve <94495911+Rodrigomonsalve@users.noreply.github.com>
2022-01-23 18:50:56 -0600

    23 de enero de 2022

---
+++ tecla-book-react/backend/model/user.js
@@ -0,0 +1,12 @@
+const sequelize = require ("../conection/db.js");
+
+module.exports.list = async () => {
+    let result = await sequelize.query("SELECT * FROM usuarios")
+    let prueba = result[0];
+    return prueba;
+}
+
+module.exports.add = async(user) => {
+    await sequelize.query(`INSERT INTO usuarios (usuario, nombre, contraseña, correo, teléfono, disponibilidad) 
+    VALUES ('${user.usuario}', '${user.nombre}', '${user.contraseña}', '${user.correo}', '${user.teléfono}', '${user.disponibilidad}')`)
+}
